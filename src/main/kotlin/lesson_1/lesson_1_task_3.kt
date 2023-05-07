4f21cc3391fef0764ded4a20ef0fcf8a1f10f9d2
StanAtilovich <alano92@yandex.ru>
2023-05-07 10:21:51 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main() {
+    val year = 1951
+    var hour = "09"
+    var minute = "07"
+    println("Год полета - $year")
+    println("Взлет - $hour:$minute")
+
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
