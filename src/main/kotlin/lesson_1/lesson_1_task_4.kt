991eed3c154a113c1dcd2c189ce50beb2a4bba9c
StanAtilovich <alano92@yandex.ru>
2023-05-07 10:26:25 +0300

    KS-1-4

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main(){
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val second: Short = 6480
+    val partOfYear: String = "2.0547945205479453E-4"
+    val apogee: Int = 327000
+
+    println("\nДанные для задачи:\nрастояние $lenght\nВозраст - $age\nЧасть дня - $partOfDay\nСекунды$second\nЧасть года - $partOfYear\nАпогей арбиты - $apogee")
+
+}
