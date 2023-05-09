5e0bc5aac31a74b574e027f09927f7ff68ba6aa1
StanAtilovich <alano92@yandex.ru>
2023-05-09 20:02:07 +0300

    l2_3

---
+++ src/main/kotlin/l2_3/l2_3.kt
@@ -0,0 +1,18 @@
+package l2_3
+
+fun main() {
+    val trainLeftHour = "9:39"
+    val trainLeft = 9 * 60 + 39
+    println("Поезд выехал в $trainLeftHour или $trainLeft минут")
+    val trainOnTheWay = 457
+    println("Поезд в пути $trainOnTheWay минут")
+    val arrivalTimeMinutes = trainLeft + trainOnTheWay
+    println("Время прибытия поезда в минутах $arrivalTimeMinutes")
+    fun minutesToHours(arrivalTimeMinutes: Int): Pair<Int, Int> {
+        val hours = arrivalTimeMinutes / 60
+        val remainder = arrivalTimeMinutes % 60
+        return Pair(hours, remainder)
+    }
+    val (hours, reminder) = minutesToHours(arrivalTimeMinutes)
+    println("Поезд приехал в $hours часов $reminder минут")
+}
